248d9ab6de2ceb5e18602300fc42e64303414646
anneyang0060 <102971671+anneyang0060@users.noreply.github.com>
2026-07-31 19:07:35 +0800

    Update Setting_FM-ID.R

--- simu/Setting_FM-ID.R
+++ simu/Setting_FM-ID.R
@@ -23,7 +23,19 @@ p <- c(p0, p0 + cumsum(delta_p))
 # p[K+1] <- 1e+04
 s <- 5:10
 s0 <- s[1]
-n0 <- 300
+
+# data-driven warm-up parameters
+warm_grid_ratio <- 1.25
+warm_min_blocks <- 2
+delta_sigma <- 0.05
+c_sigma <- 2.5
+q_cov <- 0
+R_sigma <- NULL
+warm_const <- 0.01;5e-4
+
+# hard-selection parameter
+c_h <- 2
+
 n_new <- 0 # number of obs of the new dimension
 r <- 0.01
 
@@ -44,21 +56,53 @@ delta_p <- c(p0, delta_p)
 m_dep <- 10
 idx_dep <- as.vector(sapply(cumsum(delta_p)-500, function(i){(i+1):(i+m_dep)}))
 
+block_width <- p0
+
 beta_psue <- matrix(0, length(tau), max(p))
-eff_coeff <- beta_true[idx_dep]
-for(i in 1:(length(tau)-1)){
+
+for (i in 1:(length(tau) - 1)) {
+  
   p_cur <- p[i]
-  p1 <- sum(idx_dep <= p_cur)
-  q1 <- sum(idx_dep > p_cur)
-  SX <- matrix(rho,p1,p1)
-  diag(SX) <- 1
-  SXU <- matrix(rho, p1, q1)
-  gamma <- eff_coeff[(p1+1):(p1+q1)]
-  alpha_psue <- as.vector(eff_coeff[1:p1] + ginv(SX) %*% SXU %*% gamma)
-  beta_psue[i, idx_dep[1:p1]] <- alpha_psue
-  # print(sum((alpha_psue-eff_coeff[1:p1])^2))
+  
+  idx_x <- idx_dep[idx_dep <= p_cur]
+  idx_u <- idx_dep[idx_dep > p_cur]
+  
+  if (length(idx_x) > 0) {
+    
+    if (length(idx_u) > 0) {
+      
+      idx_joint <- c(idx_x, idx_u)
+      
+      Sigma_joint <- make_chain_crossblock_cov(
+        idx_abs = idx_joint,
+        rho = rho,
+        block_width = block_width,
+        m_dep = m_dep
+      )
+      
+      p1 <- length(idx_x)
+      q1 <- length(idx_u)
+      
+      SX <- Sigma_joint[seq_len(p1), seq_len(p1), drop = FALSE]
+      SXU <- Sigma_joint[seq_len(p1), p1 + seq_len(q1), drop = FALSE]
+      
+      gamma <- beta_true[idx_u]
+      
+      alpha_psue <- beta_true[idx_x] +
+        as.vector(ginv(SX) %*% SXU %*% gamma)
+      
+    } else {
+      
+      alpha_psue <- beta_true[idx_x]
+      
+    }
+    
+    beta_psue[i, idx_x] <- alpha_psue
+  }
 }
+
 beta_psue[length(tau), ] <- beta_true
+
 K_cv <- 2
 
-prefix <- paste0('res/FM-ID_rho',rho)
+prefix <- paste0('res/FM-ID_rho',rho,'_radius',radius_mult,'_hardcv')
